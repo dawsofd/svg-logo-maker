ccd0048b6a4b4b394b68a624763af38162b82385
Dawson Dohlen <d.dohlen@pm.me>
2024-10-15 18:02:34 -0700

    add and style logo text

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 // imports
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { Square, Triangle, Circle } = require("./lib/shapes");
+const { Square, Triangle, Circle } = require("./lib/shapes.js");
 
-// write to file 
+// generate svg 
 function writeToFile(fileName, userInput) {
     let svgString = "";
     svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
     svgString += "<g>";
     svgString += `${userInput.logoShape}`;
+
+
+    // render shapes
+    let shapeChoice;
+    if (userInput.logoShape === "Square") {
+        shapeChoice = new Square();
+        svgString += `<rect x="73" y="40" width="160" height="160" fill="${userInput.shapeBackgroundColor}"/>`;
+    } else if (userInput.logoShape === "Triangle") {
+        shapeChoice = new Triangle();
+        svgString += `<polygon points= "150, 18 244, 182 56, 182" fill="${userInput.shapeBackgroundColor}"/>`;
+    } else {
+        shapeChoice = new Circle(); 
+        svgString += `<circle cx="150" cy="115" r="80" fill="${userInput.shapeBackgroundColor}"/>`;
+    }
+
+    // render + style text
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${userInput.logoTextColor}">${userInput.logoText}</text>`;
+    svgString += "</g>";
+    svgString += "</svg>";
+
+    // write output to file
+    fs.writeFile(fileName, svgString, (err) => {
+        err ? console.log(err) : console.log("Created logo.svg");
+    });
 }
 
-// render shapes
-let chooseShape;
-if (userInput.logoShape === "Square") {
-    chooseShape = new Square();
-    svgString += `<rect x="73" y="40" width="160" height="160" fill="${userInput.shapeBackgroundColor}"/>`;
-} else if (userInput.logoShape === "Triangle") {
-    chooseShape = new Triangle();
-    svgString += `<polygon points= "150, 18 244, 182 56, 182" fill="${userInput.shapeBackgroundColor}"/>`;
-} else {
-    chooseShape = new Circle(); 
-    svgString += `<circle cx="150" cy="115" r="80" fill="${userInput.shapeBackgroundColor}"/>`;
-}
+
 
 // user input
 function promptInput() {
